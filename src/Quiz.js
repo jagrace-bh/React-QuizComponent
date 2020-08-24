2581851a21bb9124744d11107a56079e945be869
Jacob Grace <jacob.grace@backstopsolutions.com>
2020-08-24 13:00:57 -0500

    PL-5: added click handling

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import QuizQuestion from "./QuizQuestion";
 import QuizEnd from "./QuizEnd";
 
 let quizData = require('./quiz_data.json')
 
-class Quiz extends Component{
+class Quiz extends Component {
 
     constructor(props) {
         super(props);
         this.state = {quiz_position: 1}
     }
 
+    showNextQuestion() {
+        this.setState({quiz_position: this.state.quiz_position + 1})
+    }
+
 
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
-        if (isQuizEnd){
+        if (isQuizEnd) {
             return <div>
                 <QuizEnd isDisabled/>
             </div>
         }
         return <div>
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+            <QuizQuestion
+                showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
         </div>
     }
 }
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,15 +3,27 @@ import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
 
+    handleClick(buttonText) {
+        if(this.props.quiz_question.answer === buttonText){
+            this.props.showNextQuestionHandler();
+        }
+    }
+
     render() {
-        return(
+        const answerList = this.props.quiz_question.answer_options.map((answer_options, index) =>
+            <QuizQuestionButton
+                clickHandler={this.handleClick.bind(this)}
+                button_text={this.props.quiz_question.answer_options[index]}
+                key={index}/>
+        );
+        return (
             <main>
                 <section>
                     <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+                        {answerList}
                     </ul>
                 </section>
             </main>
